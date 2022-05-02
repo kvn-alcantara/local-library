3690ef8b40c21b6f110bccfcf2588ec269d9f955
Kevin de Lima Alcantara <kevinalcantara37@gmail.com>
2022-05-02 11:03:24 -0300

    Add mongo connection and models

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB =
+  'mongodb+srv://root:admin@cluster0.mvwgx.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
